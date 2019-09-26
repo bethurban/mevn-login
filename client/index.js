5784f81a71c5e5cf68577eeaefede3ad3a56a591
bethurban <beth.urban@gmail.com>
2019-09-26 11:39:06 -0400

    Add Vue loader to webpack. Build Main component.

---
+++ client/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+
+import Main from './pages/Main.vue'
+
+const app = new Vue({
+
+  el: '#app',
+
+  render: h => h(Main)
+
+})
--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,36 @@ require('dotenv').config()
 
 const path = require('path')
 
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
 
   mode: process.env.NODE_ENV || 'development',
-
   entry: './client/index.js',
-
   output: {
-
     filename: 'app.js',
-
     path: path.resolve(__dirname, 'server/public')
+  },
 
-  }
+  module: {
+
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.vue$/,
+        use: {
+          loader: 'vue-loader'
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new VueLoaderPlugin()
+  ]
 }
