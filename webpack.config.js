8669b4dd3fc53d45c7d463a4f872365725e4157e
bethurban <beth.urban@gmail.com>
2019-09-26 11:03:29 -0400

    Install webpack for client, write build scripts

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const path = require('path')
+
+module.exports = {
+
+  mode: process.env.NODE_ENV || 'development',
+
+  entry: './client/index.js',
+
+  output: {
+
+    filename: 'app.js',
+
+    path: path.resolve(__dirname, 'server/public')
+
+  }
+}
